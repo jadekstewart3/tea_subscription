b3f70f2c4509324c92ff58bb49e955f000877294
Jade Stewart <jadekstewart3@gmail.com>
2023-06-06 18:13:18 -0600

    feat: add validations to tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,13 @@
 class Tea < ApplicationRecord
   has_many :subscription_teas
   has_many :subscriptions, through: :subscription_teas
+
+  validates_presence_of :title,
+                        :description,
+                        presence: true
+
+  validates_numericality_of :temperature,
+                            :brew_time,
+                            greater_than: 0
+  
 end
