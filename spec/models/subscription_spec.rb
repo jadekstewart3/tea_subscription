c1ef809d38d0a4ab1a9daad995bc495f86b09ef5
Jade Stewart <jadekstewart3@gmail.com>
2023-06-06 18:13:38 -0600

    test: add validation testing for models

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Subscription, type: :model do
   describe "relationships" do 
     it { should have_many(:teas).through(:subscription_teas) }
   end
+
+  describe "validations" do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :status }
+    it { should validate_numericality_of :price }
+    it { should validate_numericality_of :frequency }
+  end
 end
