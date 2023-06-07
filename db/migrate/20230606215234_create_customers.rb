825f9df585bf12d4fd31ed158a126b5378ff142f
Jade Stewart <jadekstewart3@gmail.com>
2023-06-06 18:11:32 -0600

    remove rouge column in customers table

--- db/migrate/20230606215234_create_customers.rb
+++ db/migrate/20230606215234_create_customers.rb
@@ -2,7 +2,6 @@ class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
       t.string :first_name
-      t.string :string
       t.string :last_name
       t.string :email
       t.string :address
