e9cc1bb1baa859e129a2109f8688868d1032f4fd
Jade Stewart <jadekstewart3@gmail.com>
2023-06-06 18:12:16 -0600

    feat: add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,21 @@
 class Customer < ApplicationRecord
   has_many :customer_subscriptions
   has_many :subscriptions, through: :customer_subscriptions
+
+  before_validation :downcase_email
+
+  validates :email, uniqueness: true,
+                    presence: true,
+                    format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :address,
+                        presence: true
+                        
+
+  private
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
