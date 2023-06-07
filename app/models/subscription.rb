1dc5dacbd5360f0f56a138d87115257bf8500800
Jade Stewart <jadekstewart3@gmail.com>
2023-06-06 18:12:46 -0600

    feat: add validations to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,13 @@
 class Subscription < ApplicationRecord
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+  enum status: [:active, :inactive]
+
+  validates_presence_of :title,
+                        :status
+                        
+  validates_numericality_of :price,
+                            :frequency,
+                            greater_than: 0
+
 end
