99203f7d1fdeb285e58b57ea2227f7ec4c554cac
Jade Stewart <jadekstewart3@gmail.com>
2023-06-07 20:06:44 -0600

    test: add relationships test for subscription

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   describe "relationships" do 
     it { should have_many(:teas).through(:subscription_teas) }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+
   end
 
   describe "validations" do 
     it { should validate_presence_of :title }
-    it { should validate_presence_of :status }
     it { should validate_numericality_of :price }
     it { should validate_numericality_of :frequency }
   end
