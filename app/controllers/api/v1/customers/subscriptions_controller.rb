ad91689baeed0e726297c8eabca73b28d679802e
Jade Stewart <jadekstewart3@gmail.com>
2023-06-08 12:00:49 -0600

    feat: error handle controllers

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -2,17 +2,22 @@ class Api::V1::Customers::SubscriptionsController < ApplicationController
   
   def create
     customer = Customer.find(params[:customer_id])
-  
     customer_subscription = customer.customer_subscriptions.new(customer_subscription_params)
     if customer_subscription.save
       render json: CustomerSubscriptionsSerializer.new(customer_subscription), status: 201
+    else
+      render json: { errors: "No matches found" }, status: :bad_request
     end
   end
 
   def update
     customer_subscription = CustomerSubscription.find_by(subscription_id: params[:id], customer_id: params[:customer_id])
-    customer_subscription.update!(customer_subscription_params)
-    render json: CustomerSubscriptionsSerializer.new(customer_subscription)
+    begin
+      customer_subscription.update!(customer_subscription_params)
+      render json: CustomerSubscriptionsSerializer.new(customer_subscription), status: 201
+    rescue ActiveRecord::RecordInvalid => error
+      render json: ErrorSerializer.new(error).error, status: 404
+    end
   end
 
   private
--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::CustomersController < ApplicationController
 
   def show
-    customer = Customer.find(params[:id])  
-  
-    render json: CustomersSerializer.new(customer), status: 200
+    begin
+      customer = Customer.find(params[:id])  
+      render json: CustomersSerializer.new(customer), status: 201
+    rescue ActiveRecord::RecordNotFound => error
+      render json: ErrorSerializer.new(error).error, status: 401
+    end
   end
 end
